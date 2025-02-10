64004270726ccf1127079062c79948b9619a4b7b
Alex Markoutsis <markout2@uwm.edu>
2025-02-10 15:58:30 -0600

    Added comment linking to google sheets with instruction reference notes, as well as register addresses. Added conditional check for number of args when executing the program.

---
+++ src/MIPSAssembler.java
@@ -0,0 +1,11 @@
+// Link to google sheet with information:
+// https://docs.google.com/spreadsheets/d/18YZ0jNXLmXgINMvws57R3akAFXtTHVRhdQyYqoGsuJ0/edit?usp=sharing
+
+public class MIPSAssembler {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java MIPSAssembler <MIPS instructions>");
+            System.exit(1);
+        }
+    }
+}
