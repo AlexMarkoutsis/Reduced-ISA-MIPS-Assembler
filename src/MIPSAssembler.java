f19deb20658f590a395bc94b1d0f9d72f81188dc
Alex Markoutsis <markout2@uwm.edu>
2025-02-14 12:27:21 -0600

    Added register map, opcode map, and funct map. Added implementation logic for r types, seem to be fully working at this time, but further testing is required before submission

--- src/MIPSAssembler.java
+++ src/MIPSAssembler.java
@@ -2,8 +2,50 @@
 // https://docs.google.com/spreadsheets/d/18YZ0jNXLmXgINMvws57R3akAFXtTHVRhdQyYqoGsuJ0/edit?usp=sharing
 
 import java.util.Arrays;
+import java.util.List;
 
 public class MIPSAssembler {
+    // Registers and their addresses
+    static String[] registers = {
+            "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3",
+              "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
+              "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
+              "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"
+    };
+
+    // Instructions based on their types
+    static String[] RTypes = {"add", "and", "or", "slt", "sub"};
+    static String[] ITypes = {"addiu", "andi", "beq", "bne", "lui", "lw", "ori", "sw"};
+
+    // Arrays for finding value based on instruction
+    List<String> opcodeMap = Arrays.asList(new String[64]);
+    List<String> functMap = Arrays.asList(new String[64]);
+
+    // Initializes values in arrays (Constructor)
+    public MIPSAssembler() {
+        // I-Type instructions
+        opcodeMap.set(4, "beq");
+        opcodeMap.set(5, "bne");
+        opcodeMap.set(9, "addiu");
+        opcodeMap.set(12, "andi");
+        opcodeMap.set(13, "ori");
+        opcodeMap.set(15, "lui");
+        opcodeMap.set(35, "lw");
+        opcodeMap.set(43, "sw");
+
+        // J-Type instructions
+        opcodeMap.set(2, "j");
+
+        // R-Type instructions
+        functMap.set(12, "syscall");
+        functMap.set(32, "add");
+        functMap.set(34, "sub");
+        functMap.set(36, "and");
+        functMap.set(37, "or");
+        functMap.set(42, "slt");
+    }
+
+
     public static void main(String[] args) {
         // Invalid usage check
         if (args.length != 1) {
@@ -14,32 +56,22 @@ public class MIPSAssembler {
         // Cut off comments
         String instruction = args[0];
         int commentIndex = instruction.indexOf("#");
-        if (commentIndex != -1) { instruction = instruction.substring(0, commentIndex); }
+        if (commentIndex != -1) {
+            instruction = instruction.substring(0, commentIndex);
+        }
         // Remove whitespace from front and back
         instruction = instruction.trim();
 
-        // DEBUGGING
-        // System.out.println(instruction);
-
-        // Call main helper function, and store output in an int
         int machineCode = assembleMIPS(instruction);
         System.out.printf("%08x%n", machineCode);  // Convert to hexadecimal
-
-
-        // Also need a way to attach registers to their binary
-        // ^^Same with opcode/funct... maybe use a Map?
     }
 
 
     private static int assembleMIPS(String instruction) {
-        String[] RTypes = {"add", "and", "or", "slt", "sub"};
-        String[] JTypes = {"j", "syscall"};
-        String[] ITypes = {"addiu", "andi", "beq", "bne", "lui", "lw", "ori", "sw"};
-
         // regex:
-        // "?:" prevents creation  of empty elements
-        // ",\\s" matches a comma or whitespace
-        // "+" matches one or more occurrences of wither
+        // "?:"     prevents creation  of empty elements
+        // ",\\s"   matches a comma or whitespace
+        // "+"      matches one or more occurrences of wither
         String regex = "(?:,|\\s)+";
         String[] parts = instruction.split(regex);
 
@@ -51,10 +83,6 @@ public class MIPSAssembler {
             if ((tickIndex != -1 || slashIndex != -1) && parts[i].length() > 1) {
                 parts[i] = parts[i].substring(1, parts[i].length() - 1);
             }
-
-            // DEBUGGING
-            // System.out.printf("parts[%d] = ", i);
-            // System.out.println(parts[i]);
         }
 
         String opcode = parts[0].toLowerCase();
@@ -80,15 +108,15 @@ public class MIPSAssembler {
     // shamt is always 00000
     // funct defines what operation is done
     // use bit shifting to shift parts into their correct position
-    private static int assembleRType(String[] RENAME) {
-        int opcode = 0;
-        int rs = 0;
-        int rt = 0;
-        int rd = 0;
-        int shamt = 0;
-        int funct = 0;
-        // return opcode | rs | rt | rd | shamt | funct;
-        return 1;
+    private static int assembleRType(String[] parts) {
+        MIPSAssembler assembler = new MIPSAssembler();
+
+        int rs = Arrays.asList(registers).indexOf(parts[2]);
+        int rt = Arrays.asList(registers).indexOf(parts[3]);
+        int rd = Arrays.asList(registers).indexOf(parts[1]);
+        int funct = assembler.functMap.indexOf(parts[0]);
+
+        return (rs << 21) | (rt << 16) | (rd << 11) | funct;
     }
 
 
